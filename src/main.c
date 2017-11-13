855f169a4476537c72628a39bca73e516a02e03e
Kushami Neko <missing10630@gmail.com>
2017-11-13 11:47:21 +0900

    fix memory leak

--- src/main.c
+++ src/main.c
@@ -10,22 +10,26 @@
 #define FULL_WIDTH 1920
 #define FULL_HEIGHT 1080
 
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 450
+#define START_X 0
+#define START_Y 505
 
-#define FACTOR 15
-#define ZOOM_WIDTH 16 * FACTOR
-#define ZOOM_HEIGHT 9 * FACTOR
+#define WINDOW_FACTOR 28
+#define WINDOW_WIDTH 16 * WINDOW_FACTOR
+#define WINDOW_HEIGHT 9 * WINDOW_FACTOR
 
-#define STEP 10
-#define INTERVAL 100
+#define ZOOM_FACTOR 13
+#define ZOOM_WIDTH 16 * ZOOM_FACTOR
+#define ZOOM_HEIGHT 9 * ZOOM_FACTOR
+
+#define STEP 5
+#define INTERVAL 150
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 static GdkPixbuf *SCREENSHOT = NULL;
 
-static gint srcX = FULL_WIDTH - ZOOM_WIDTH - 70;
-static gint srcY = 277;
+static gint srcX = FULL_WIDTH - ZOOM_WIDTH - 160;
+static gint srcY = 250;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -62,13 +66,6 @@ static GdkPixbuf *screenshotZoom() {
 
   return screenshot;
 }
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-struct KeyPressedData {
-  GtkWindow *window;
-  GtkImage *image;
-};
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -117,7 +114,13 @@ static gboolean cbRender(void *data) {
 
   GtkImage *mainImage = (GtkImage *)data;
 
+  GdkPixbuf *screenOld = gtk_image_get_pixbuf(mainImage);
+
   gtk_image_clear(mainImage);
+
+  if (screenOld != NULL) {
+    g_object_unref(screenOld);
+  }
 
   screenshotTake();
   GdkPixbuf *screen = screenshotZoom();
@@ -130,24 +133,19 @@ static gboolean cbRender(void *data) {
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-static void *activate() {
+static void activate() {
 
   GtkWidget *mainWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title(GTK_WINDOW(mainWindow), "Screen Magnifier");
   gtk_window_set_resizable(GTK_WINDOW(mainWindow), FALSE);
 
-  gtk_window_set_gravity(GTK_WINDOW(mainWindow), GDK_GRAVITY_SOUTH_EAST);
-  gtk_window_move(GTK_WINDOW(mainWindow), FULL_WIDTH, FULL_HEIGHT);
+  gtk_window_set_gravity(GTK_WINDOW(mainWindow), GDK_GRAVITY_NORTH_WEST);
+  gtk_window_move(GTK_WINDOW(mainWindow), START_X, START_Y);
   gtk_window_set_keep_above(GTK_WINDOW(mainWindow), TRUE);
 
   GtkWidget *mainImage = gtk_image_new_from_pixbuf(NULL);
 
   gtk_container_add(GTK_CONTAINER(mainWindow), mainImage);
-
-  struct KeyPressedData *data = malloc(sizeof(struct KeyPressedData));
-
-  data->window = GTK_WINDOW(mainWindow);
-  data->image = GTK_IMAGE(mainImage);
 
   g_signal_connect(GTK_WIDGET(mainWindow), "destroy", G_CALLBACK(gtk_main_quit),
                    NULL);
@@ -158,8 +156,6 @@ static void *activate() {
   g_timeout_add(INTERVAL, cbRender, mainImage);
 
   gtk_widget_show_all(GTK_WIDGET(mainWindow));
-
-  return data;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -168,11 +164,9 @@ int main(int argc, char **argv) {
 
   gtk_init(&argc, &argv);
 
-  void *data = activate();
+  activate();
 
   gtk_main();
-
-  free(data);
 
   return 0;
 }
